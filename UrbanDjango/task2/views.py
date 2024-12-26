e7d844a4f492c6e77a9dc1751e8a86b610bda2d6
Максим <maxkurylevv@gmail.com>
2024-12-26 17:42:03 +0700

    Module_18_3

--- UrbanDjango/task2/views.py
+++ UrbanDjango/task2/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateV
 def func_temp(request):
     return render(request, 'func_template.html')
 
 class class_temp(TemplateView):
     template_name = 'class_template.html'
 # Create your views here.
-git
